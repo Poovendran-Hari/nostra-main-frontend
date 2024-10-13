6e02c32bb006d46bff2730681a4eef438b61f319
Poovendran-Hari <hustlesmarket@gmail.com>
2024-10-13 21:30:03 +0530

    updated

---
+++ index.js
@@ -0,0 +1,6 @@
+var banner = document.getElementById("block1__banner")
+var closenav = document.getElementById("closenav")
+closenav.addEventListener("click",function(){
+    banner.style.display="none"
+}
+)
